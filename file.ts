23c3eff7d0fba581eee41b4b516ec8d861d0d204
unknown <HVsystem@winlenovo.cern.ch>
2022-01-25 19:15:17 +0100

    ffff

---
+++ file.ts
@@ -0,0 +1,27 @@
+// enum Direction {
+//     up = 5,
+//     down,
+//     left,
+//     right,
+// }
+// console.log(Direction);
+
+// enum UserResponse {
+//     No = 0,
+//     Yes = 1,
+// }
+
+// function respond(rec:string, messs:UserResponse): void {}
+   
+// respond("Princess Caroline", UserResponse.Yes);
+
+// enum E2 {
+//     A = 1,
+//     B,
+//     C,
+// }
+// console.log(E2.B);
+
+console.log(5==6 || 4==3);
+
+
